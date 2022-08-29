f2bf572b55001f24f12d93e0e49819b2d9991c83
DiChag <chagindmitry1@gmail.com>
2022-08-29 13:52:30 +0300

    the-end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = createElements(galleryItems);
+galleryContainer.addEventListener('click', openModal);
+
+function openModal(evt) {
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  evt.preventDefault();
+  const origImg = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="1280" height="854">
+`);
+  origImg.show();
+  window.addEventListener('keydown', closeModal);
+
+  function closeModal(evt) {
+    if (evt.code === 'Escape') {
+      origImg.close();
+      window.removeEventListener('keydown', closeModal);
+    }
+  }
+}
+
+function createElements(gallery) {
+  const imgEl = gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+         <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+    </div>`
+    )
+    .join('');
+  return imgEl;
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('ul.gallery');
+
+galleryContainer.innerHTML = creatElements(galleryItems);
+
+function creatElements(gallery) {
+  const imgEl = gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+    )
+    .join('');
+
+  return imgEl;
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
